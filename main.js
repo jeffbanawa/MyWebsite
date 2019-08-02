59890b13d02fb03728186f45b2e4ca3eba34df17
Jeffbanawa <jeffrey.banawa@gmail.com>
2019-08-01 18:01:29 -0600

    Enter on scroll effect

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 var controller = new ScrollMagic.Controller();
 
-var scene = new ScrollMagic.Scene({
+new ScrollMagic.Scene({
     triggerElement: '.flip-card',
     reverse: false
 })
 .setClassToggle('.flip-card', 'show')
 .addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: ".project-content",
+    reverse: false
+})
+    .setClassToggle(".project-content", "show") // add class to reveal
+    .addTo(controller);

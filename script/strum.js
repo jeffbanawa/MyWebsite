c442203b881687f5239e5b6e37a2944b17aedb15
Jeffbanawa <jeffrey.banawa@gmail.com>
2021-06-09 02:21:02 -0600

    add strings

--- script/strum.js
+++ script/strum.js
@@ -5,27 +5,26 @@ let coords = {
   y: 0,
 };
 
-let width = 0;
+let length = 0;
 
 document.addEventListener("mousemove", (event) => {
   coords.x = event.clientX;
   coords.y = event.clientY;
 
-  width = coords.x / window.innerWidth;
+  length = coords.y / window.innerHeight;
 
-  console.log(width.toFixed(1));
-
+  console.log(length.toFixed(1));
   updateCoords(coords);
 });
 
 document.addEventListener("mouseout", (event) => {
   gsap.to(path, {
     ease: Elastic.easeOut.config(1, 0.3),
-    attr: { d: "M168,495 Q362,494 1385,495" },
+    attr: { d: "M168,495 Q362,495 1385,495" },
   });
 });
 
 function updateCoords(coords) {
-  coords.x = width * 10;
+  coords.y = length * 1080;
   path.setAttribute("d", `M168,495 Q${coords.x},${coords.y} 1385,495`);
 }
